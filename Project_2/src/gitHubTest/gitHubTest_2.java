4ea32d99db04c0e422a7c64e23ac7bca437d822a
zepho <zepho@10.150.22.23>
2023-01-27 16:39:05 -0500

    Added a counter of up to 9 (Stops at less than 10 is 9)

--- Project_2/src/gitHubTest/gitHubTest_2.java
+++ Project_2/src/gitHubTest/gitHubTest_2.java
@@ -4,6 +4,10 @@ class gitHubTest_2 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Friends this is my new code");
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println("The current index is: " + i);
+		}
 	}
 
 }
