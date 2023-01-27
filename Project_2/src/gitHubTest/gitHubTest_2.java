16e5f430fac62111786947f0dfe6c67b6aa4f3df
zepho <zepho@10.150.22.23>
2023-01-27 16:41:00 -0500

    hello friends

--- Project_2/src/gitHubTest/gitHubTest_2.java
+++ Project_2/src/gitHubTest/gitHubTest_2.java
@@ -6,7 +6,13 @@ class gitHubTest_2 {
 		System.out.println("Hello Friends this is my new code");
 		
 		for (int i = 0; i < 10; i++) {
-			System.out.println("The current index is and counting is: " + i);
+			if (i < 10) {
+				System.out.println("The current index is and counting is: " + i);
+			} else {
+				System.out.println("The current index is in the else statement");
+			}
+			
+			
 		}
 	}
 
